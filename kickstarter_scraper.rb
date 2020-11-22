00c029483554fb772cd955798e2e58383e1c2c28
mccray austin <tubach75@hotmail.com>
2020-11-22 14:18:06 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,12 +7,12 @@ require "pry"
   # image link: project.css("div.project-thumbnail a img").attribute("src").value
   # location: project.css("ul.project-meta span.location-name").text
   # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
-  
+
 def create_project_hash
 
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
-
+  binding.pry
   projects = {}
 
   kickstarter.css("li.project.grid_4").each do |project|
@@ -28,3 +28,5 @@ def create_project_hash
   projects
 
 end
+
+create_project_hash
